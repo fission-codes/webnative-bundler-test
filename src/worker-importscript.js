51a6cef83a0765d7cbfb69ba772d290c00c91f6d
Steven Vandevelde <icid.asset@gmail.com>
2022-08-24 13:30:46 +0200

    Add workers

---
+++ src/worker-importscript.js
@@ -0,0 +1,28 @@
+importScripts("vendor/webnative/index.umd.min.js")
+
+const wn = webnative
+
+  ; (async () => {
+    const fs = await wn.fs.empty({ localOnly: true })
+    const privatePath = wn.path.file(
+      wn.path.Branch.Private,
+      "testing",
+      "write.txt"
+    )
+
+    const publicPath = wn.path.directory(
+      wn.path.Branch.Public,
+      "testing",
+      "nested"
+    )
+
+    await fs.write(privatePath, "contents")
+    const contents = await fs.read(privatePath)
+
+    console.log(contents)
+    console.log(await fs.ls(wn.path.parent(privatePath)))
+
+    await fs.mkdir(publicPath)
+
+    console.log(await fs.ls(wn.path.parent(publicPath)))
+  })()

97ffc22dade5be8d4d566227bf7cce8c376484a2
Steven Vandevelde <icid.asset@gmail.com>
2022-08-25 17:24:35 +0200

    0.34.0-alpha-5

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,1 @@
-export default {
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        "global": "globalThis",
-        "globalThis.process.env.NODE_ENV": "production",
-      },
-      target: "es2020"
-    }
-  },
-  build: {
-    target: "es2020"
-  },
-}
+export default {}
